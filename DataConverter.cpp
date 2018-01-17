10ed708dd72494cd480a89da17045652a2c427ab
Nunzio <nunzio03@hotmail.it>
2018-01-17 14:05:50 +0100

    added some printing instructions in order to verify the correct conversion of CanBus data before pubblishing via mqtt

--- DataConverter.cpp
+++ DataConverter.cpp
@@ -19,6 +19,7 @@
 #include <json/json.h>
 #include <sstream>
 #include <math.h>
+#include <iostream>
 
 DataConverter::DataConverter() : mosqpp::mosquittopp()
 {
@@ -68,7 +69,7 @@ std::string DataConverter::Cnvt_Data2Json(const struct Data fmt)
 	std::stringstream buf;
 
 	buf << "{\"time\":" << fmt.time << ",\"value\":" << fmt.value << "}";
-	// std::cout<< buf.str() << std::endl; // DEBUG PURPOSES
+	//qui debuggavo
 	return buf.str();
 }
 
@@ -76,6 +77,7 @@ void DataConverter::Pub(std::string channel, const struct Data fmt)
 {
 	std::string json = Cnvt_Data2Json(fmt);
 	this->publish(NULL, channel.c_str(), json.length(), json.c_str());
+
 }
 
 void DataConverter::Fmt(struct CANFrame frame)
@@ -90,108 +92,204 @@ void DataConverter::Fmt(struct CANFrame frame)
 		case 747:
 		channelName = "a1x";
 		value = ((frame.data[0] * 3.0) / 255.0) - 1.5 + (frame.data[1] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "a2x";
 		value = ((frame.data[2] * 3.0) / 255.0) - 1.5 + (frame.data[3] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "a3x";
 		value = ((frame.data[4] * 3.0) / 255.0) - 1.5 + (frame.data[5] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "a1y";
 		value = ((frame.data[6] * 3.0) / 255.0) - 1.5 + (frame.data[7] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 748:
 		channelName = "a2y";
 		value = ((frame.data[0] * 3.0) / 255.0) - 1.5 + (frame.data[1] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "a3y";
 		value = ((frame.data[2] * 3.0) / 255.0) - 1.5 + (frame.data[3] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 749:
 		channelName = "a1z";
 		value = ((frame.data[0] * 3.0) / 255.0) - 1.5 + (frame.data[1] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "a2z";
 		value = ((frame.data[2] * 3.0) / 255.0) - 1.5 + (frame.data[3] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "a3z";
 		value = ((frame.data[4] * 3.0) / 255.0) - 1.5 + (frame.data[5] / 1000.0);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 750:
 		channelName = "slip_ok";
 		value = frame.data[7];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "auto_acc_flag";
 		value = (frame.data[0] & 128) / 128;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "debug_mode";
 		value = (frame.data[0] & 64) / 64;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "datalog_on-off";
 		value = (frame.data[0] & 32) / 32;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "telemetria_on-off";
 		value = (frame.data[0] & 16) / 16;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "lap_close";
 		value = frame.data[1];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 751:
 		channelName = "target_rpm_up";
 		value = (frame.data[0] * 256.0) + frame.data[1];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "shift_duration";
 		value = (frame.data[2] * 256.0) + frame.data[3];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "time_0-100";
 		value = (frame.data[4] * 256.0) + frame.data[5];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "time_0-75";
 		value = (frame.data[6] * 256.0) + frame.data[7];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 752:
 		channelName = "altitude";
 		value = ((frame.data[0] * 256.0) + frame.data[1]) / 10.0;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "gps_speed";
 		value = ((frame.data[2] * 256.0) + frame.data[3]) / 10.0;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "satellite";
 		value = frame.data[4];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "fix";
 		value = frame.data[5];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "gps_time";
 		value = (frame.data[6] * 256.0) + frame.data[7];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
@@ -199,6 +297,10 @@ void DataConverter::Fmt(struct CANFrame frame)
 		channelName = "latitude";
 		framedata = frame.data;
 		value = *((double*)framedata);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
@@ -206,130 +308,232 @@ void DataConverter::Fmt(struct CANFrame frame)
 		channelName = "longitude";
 		framedata = frame.data;
 		value = *((double*)framedata);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 768:
 		channelName = "rpm";
 		value = (frame.data[0] * 256.0) + frame.data[1];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "tps";
 		value = (frame.data[2] * 100.0) / 255.0;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "map";
 		value = (frame.data[4] * 256.0) + frame.data[5];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "lambda";
 		value = (frame.data[6] * 256.0) + frame.data[7];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 769:
 		channelName = "derivata_farfalla";
 		value = (frame.data[0] * 256.0) + frame.data[1];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "dmap";
 		value = (frame.data[2] * 256.0) + frame.data[3];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 770:
 		channelName = "vhspeed";
 		value = ((frame.data[0] * 256.0) + frame.data[1]) * 0.1;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "draxspeed";
 		value = ((frame.data[2] * 256.0) + frame.data[3]) * 0.1;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "slip_calc";
 		value = ((frame.data[4] * 256.0) + frame.data[5]) * 50.0 / 255.0;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "osasplip";
 		value = ((frame.data[6] * 256.0) + frame.data[7]) * 0.25;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 772:
 		channelName = "terogpianoquotato";
 		value = ((frame.data[0] * 256.0) + frame.data[1]);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "terog";
 		value = ((frame.data[2] * 256.0) + frame.data[3]);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "sabase";
 		value = ((frame.data[4] * 256.0) + frame.data[5]) * 0.25;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "sa";
 		value = ((frame.data[6] * 256.0) + frame.data[7]) * 0.25;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 774:
 		channelName = "gear";
 		value = frame.data[1];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "gear_shift_time_remain";
 		value = ((frame.data[2] * 256.0) + frame.data[3]);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "poil";
 		value = ((frame.data[4] * 256.0) + frame.data[5]);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "pfuel";
 		value = ((frame.data[6] * 256.0) + frame.data[7]);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 775:
 		channelName = "baro";
 		value = ((frame.data[0] * 256.0) + frame.data[1]);
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "lnr3i";
 		value = (frame.data[2] * 256.0) + frame.data[3];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "lnr4i";
 		value = (frame.data[4] * 256.0) + frame.data[5];
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 776:
 		channelName = "vbattdir";
 		value = ((frame.data[0] * 256.0) + frame.data[1]) * 18.0 / 1024.0;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "vbattkey";
 		value = ((frame.data[2] * 256.0) + frame.data[3]) * 18.0 / 1024.0;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 
 		case 779:
 		channelName = "th20";
 		value = (((frame.data[0] * 256.0) + frame.data[1]) * 160.0 / 65535.0) - 10.0;
+
+		 std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "toil";
 		value = ((frame.data[2] * 256.0) + frame.data[3]) * 18.0 / 1024.0;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "tmp";
 		value = ((frame.data[4] * 256.0) + frame.data[5]) * 18.0 / 1024.0;
+
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES		
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 
 		channelName = "tair";
 		value = ((frame.data[6] * 256.0) + frame.data[7]) * 18.0 / 1024.0;
+		
+		std::cout<< channelName<<": " <<" "<<value << std::endl; // DEBUG PURPOSES
+
 		this->Pub(channelPrefix + channelName, {frame.time, value});
 		break;
 	}
